f92bf55f838b3fc4a429caefdaa28728b1f773d5
PastorAmin <pastoramin19@gmail.com>
2022-08-14 01:17:09 +0000

    line added from local

--- first-code.py
+++ first-code.py
@@ -2,4 +2,4 @@ import datetime
 now = datetime.datetime.now()
 print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
-print ("I am learning GIT")
+print ("I am learning GIT on AWS and local server")
